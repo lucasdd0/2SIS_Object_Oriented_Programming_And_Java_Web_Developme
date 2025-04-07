93babeb3eb82953c954d6b824530165c998a8c13
lucasdd0 <lucasfo2005@gmail.com>
2025-04-07 19:49:06 -0300

    commit

---
+++ PrimeiroSemestre/src/Heranca/Empregado.java
@@ -0,0 +1,7 @@
+package Heranca;
+
+public class Empregado {
+    long matricula;
+    String nome;
+
+}
---
+++ PrimeiroSemestre/src/Heranca/EmpregadoComissionado.java
@@ -0,0 +1,6 @@
+package Heranca;
+
+public class EmpregadoComissionado extends Empregado {
+}
+
+
